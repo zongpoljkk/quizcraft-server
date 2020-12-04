6d7d25d42f1d3f85beace1fb30624c8f4057f4af
Zongpol <levatinez@gmail.com>
2020-12-05 01:48:47 +0700

    [C-12]: WIP

--- controllers/answer.js
+++ controllers/answer.js
@@ -1,7 +1,14 @@
 const Answer = require("../models/Answer");
 
 exports.getAnswer = (req, res, next) => {
-    answer.save();
+    // answer.save();
+    console.log(req.body.problem_id)
+    const problem_id = req.body.problem_id;
+    Answer.findById(problem_id).then((err, answer) => {
+      // console.log(err)
+      console.log(answer)
+    })
+    
 
     res.json(answer);
 }
@@ -10,7 +17,7 @@ exports.postAnswer = (req, res, next) => {
   console.log("KAOO")
 //   console.log(req);
   console.log(req.body)
-  console.log(req.body.id)
+  console.log(req.body.problem_id)
   console.log(req.body.content)
   const id = req.body.id;
   const content = req.body.content;
--- models/Answer.js
+++ models/Answer.js
@@ -19,7 +19,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const answerSchema = new Schema({
-    question_id: {
+    problem_id: {
         type: String,
         required: true,
     },
