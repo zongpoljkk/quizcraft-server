5c5fdf75bc2c137860596669877c6393e0b0a91e
Jinwara <naon123@hotmail.com>
2020-12-06 16:49:36 +0700

    reindent using 2 spaces

--- models/User.js
+++ models/User.js
@@ -2,55 +2,55 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    username: {
-        type: String,
-    },
-    school: {
-        type: String,
-        required: true,
-    },
-    class: {
-        type: String,
-        required: true,
-    },
-    rank: {
-        type: String,
-        required: true,
-        enum: ['BRONZE','SILVER','GOLD','DIAMOND'],
-    },
-    level: {
-        type: Number,
-        required: true,
-    },
-    coin: {
-        type: Number,
-        required: true,
-    },
-    photo: {
-        type: String,
-    },
-    smartSchoolAccount: {
-        type: String,
-    },
-    streak: {
-        type: Number,
-        required: true,
-    },
-    items: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Item',  
-    }],
-    archievements: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Archievement'
-    }],
-    tips: [{
-        type: String,
-    }]
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+  },
+  school: {
+    type: String,
+    required: true,
+  },
+  class: {
+    type: String,
+    required: true,
+  },
+  rank: {
+    type: String,
+    required: true,
+    enum: ['BRONZE','SILVER','GOLD','DIAMOND'],
+  },
+  level: {
+    type: Number,
+    required: true,
+  },
+  coin: {
+    type: Number,
+    required: true,
+  },
+  photo: {
+    type: String,
+  },
+  smartSchoolAccount: {
+    type: String,
+  },
+  streak: {
+    type: Number,
+    required: true,
+  },
+  items: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Item',  
+  }],
+  archievements: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Archievement'
+  }],
+  tips: [{
+    type: String,
+  }]
 });
 
 module.exports = mongoose.model('User', UserSchema);
