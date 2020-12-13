022fe30a166e3732620fd069b2899f5d74040291
Zongpol <levatinez@gmail.com>
2020-12-13 12:19:18 +0700

    Return all three Global, School, Classroom including the index of the user

--- controllers/user.js
+++ controllers/user.js
@@ -3,49 +3,84 @@ const User = require("../models/User");
 exports.getLeaderBoard = async (req, res, next) => {
   const userId = req.query.userId;
   console.log(userId);
-  User.findById(userId)
-    .select("_id school class")
-    .exec((err, user) => {
+
+  // ? Global ? //
+  User.find({})
+    .select("_id, username level class")
+    .exec((err, users) => {
       if (err) {
-        console.log(`errrrror`);
         res.status(500).send({ error: err });
-      } else if (!user) {
-        res.status(400).send("The user with the given userId was not found");
+      } else if (!users) {
+        res.status(400).send("Unable to get all users");
         return;
       }
-      const classroom = user.class;
-      const school = user.school;
-      // * Same School * //
-      User.find({ school: school })
-        .select("_id username level class")
-        .exec((err, users) => {
+
+      let copyUsers = users.slice(0);
+      const byAll = copyUsers.sort((a, b) => {
+        return b.level - a.level;
+      });
+      const indexGlobal = byAll.findIndex((user) => {
+        return user._id == userId;
+      });
+
+      User.findById(userId)
+        .select("_id school class")
+        .exec((err, user) => {
           if (err) {
             res.status(500).send({ error: err });
           } else if (!user) {
-            res.status(400).send("The school name was not found");
+            res
+              .status(400)
+              .send("The user with the given userId was not found");
             return;
           }
+          const classroom = user.class;
+          const school = user.school;
 
-          let copyUsers = users.slice(0);
-          const bySchool = copyUsers.sort((a, b) => {
-            return b.level - a.level;
-          });
-          console.log(bySchool);
+          // * Same School * //
+          User.find({ school: school })
+            .select("_id username level class")
+            .exec((err, users) => {
+              if (err) {
+                res.status(500).send({ error: err });
+              } else if (!user) {
+                res
+                  .status(400)
+                  .send("The user with the given school name was not found");
+                return;
+              }
 
-          const classroomUsers = copyUsers.filter((user) => {
-            return user.class === classroom;
-          });
+              let copyUsers = users.slice(0);
+              const bySchool = copyUsers.sort((a, b) => {
+                return b.level - a.level;
+              });
 
-          const byClassroom = classroomUsers.sort((a, b) => {
-            return b.level - a.level;
-          });
+              const indexSchool = bySchool.findIndex((user) => {
+                return user._id == userId;
+              });
 
-          console.log(byClassroom);
+              const classroomUsers = copyUsers.filter((user) => {
+                return user.class === classroom;
+              });
 
-          res.send(bySchool);
+              const byClassroom = classroomUsers.sort((a, b) => {
+                return b.level - a.level;
+              });
+
+              const indexClassroom = byClassroom.findIndex((user) => {
+                return user._id == userId;
+              });
+
+              res.send({
+                byAll: byAll,
+                bySchool: bySchool,
+                byClassroom: byClassroom,
+                indexGlobal: indexGlobal,
+                indexSchool: indexSchool,
+                indexClassroom: indexClassroom,
+              });
+              next();
+            });
         });
-
-      // res.send(user);
-      // next();
     });
 };
