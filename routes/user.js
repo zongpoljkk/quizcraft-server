8b83365054ab6d4ee715594c9ac034f1f7908d51
nnnnnnnutt <pimkunut.tee@gmail.com>
2020-12-20 20:46:02 +0700

    [C2-14] add authjwt to user route

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const userController = require('../controllers/user');
 
+const { authJwt, adminOnly } = require('../middlewares');
+
 const router = express.Router();
 
-router.get('/', userController.getAllUsers)
-router.get('/get-user/', userController.getProfileByUID)
-router.put('/edit-username', userController.editUsername)
-router.post('/add-user', userController.addUser)
-router.put('/used-item', userController.usedItem)
+router.get('/', [authJwt], userController.getAllUsers)
+router.get('/get-user/', [authJwt], userController.getProfileByUID)
+router.put('/edit-username', [authJwt], userController.editUsername)
+router.post('/add-user', [authJwt, adminOnly], userController.addUser)
+router.put('/used-item', [authJwt], userController.usedItem)
 
 module.exports = router;
