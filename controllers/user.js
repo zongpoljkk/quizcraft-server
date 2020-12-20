652ef68af5df5af785b82fd292b411be68151ba9
mingokl02135 <mingbudsakorn@gmail.com>
2020-12-21 00:37:37 +0700

    edit to return response and update lastlogin time

--- controllers/user.js
+++ controllers/user.js
@@ -146,10 +146,9 @@ exports.editUsername = async (req, res) => {
 });
 };  
 
-
 exports.updateStreak = async (userId) => {
-  const user = await User.findOne({_id:userId});
   const now = new Date();
+  const user = await User.findOneAndUpdate({_id:userId}, {lastLogin: now});
   const lastLoginDate = new Date(user.lastLogin);
   const nextDate = new Date(user.lastLogin);
   nextDate.setDate(nextDate.getDate() + 1);
