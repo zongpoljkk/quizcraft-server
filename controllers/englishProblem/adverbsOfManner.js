aadf5eb71fa99287163cc47dd1c1143af58a6062
mingokl02135 <mingbudsakorn@gmail.com>
2020-12-21 01:59:14 +0700

    .

--- controllers/englishProblem/adverbsOfManner.js
+++ controllers/englishProblem/adverbsOfManner.js
@@ -7,7 +7,7 @@ const EASY = "EASY";
 const MEDIUM = "MEDIUM";
 const HARD = "HARD";
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
-var sentences = [`She agreed to re-type the letter quickly.`]
+var sentences = [`She agreed to re-type the letter quickly.`,`Evie can play tennis and badminton but she prefers team sports.`]
 
 const wordpos = new WordPOS();
 
@@ -17,21 +17,26 @@ const randInt = (start, end) => {
 
 const generateAdverbsOfManner = async (subtopicName, difficulty) => {
   var out = '';
-  var adverb;
-  var sentenceIndex = randInt(0,sentences.length)
+  var adverb,adj,verbs;
+  var sentenceIndex = randInt(0,sentences.length-1)
   switch (difficulty) {
     case EASY:
       try {
-        adverb = await wordpos.getAdverbs(sentences[0])
-        syn = synonyms(adverb[0])
+        // adverbs = await wordpos.getPOS(sentences[0])
+        verbs = await wordpos.getVerbs(sentences[sentenceIndex])
+        adverbs = await wordpos.getAdverbs(sentences[sentenceIndex])
+        adj  = await wordpos.randAdjective({startsWith:adverbs[0][0]})
+        // syn = synonyms(adverb[randInt(0,adverbs.length-1)])
       }catch(err){
         console.log(err)
       }
-      console.log(adverb)
-      console.log(syn)
+      console.log('verbs',verbs)
+      console.log('adverbs',adverbs)
+      console.log('adj',adj)
+      // console.log(syn)
       break;
   }
 };
-// generateAdverbsOfManner('test','EASY')
-// return 0
+generateAdverbsOfManner('test','EASY')
+return 0
 module.exports = { generateAdverbsOfManner }
