b209979fc31905b8a83af4726713031db57b3e9e
Zongpol <levatinez@gmail.com>
2020-12-22 18:02:54 +0700

    Add authJwt for leaderboard route

--- routes/leaderBoard.js
+++ routes/leaderBoard.js
@@ -2,9 +2,15 @@ const express = require("express");
 
 const leaderBoardController = require("../controllers/leaderBoard");
 
+const { authJwt } = require("../middlewares");
+
 const router = express.Router();
 
 // /leader-board/get-leader-board => GET
-router.get("/get-leader-board", leaderBoardController.getLeaderBoard);
+router.get(
+  "/get-leader-board",
+  [authJwt],
+  leaderBoardController.getLeaderBoard
+);
 
 module.exports = router;
