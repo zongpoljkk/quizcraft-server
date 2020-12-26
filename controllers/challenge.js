84926e40700368d31312cfc1cd00f7fd5a76c252
mingokl02135 <mingbudsakorn@gmail.com>
2020-12-27 03:56:41 +0700

    init

---
+++ controllers/challenge.js
@@ -0,0 +1,8 @@
+const User = require("../models/User");
+const Challenge = require("../models/Challenge");
+
+exports.randomChallenge = async (req, res) => {
+  //step1: random user2 that user2 != user1
+  return res.send("TO DO")
+
+}
