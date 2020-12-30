46667130cc030097666fa70ee4630a28f42945b2
Zongpol <levatinez@gmail.com>
2020-12-30 18:51:10 +0700

    WIP

--- controllers/challenge.js
+++ controllers/challenge.js
@@ -12,24 +12,31 @@ exports.randomChallenge = async (req, res) => {
   const subject = req.body.subject;
   const subtopicName = req.body.subtopicName;
   const difficulty = req.body.difficulty;
-  var problem,problems = [];
+  var problem,
+    problems = [];
   try {
     //step1: random user2 that user2 != user1
-    const user1 = await User.findOne({ _id: user1Id }, { _id:1, firstname:1, lastname:1, username:1, photo:1 });
-    let count = await User.countDocuments({_id: {$ne: user1Id}});
-    let random = randInt(0,count-1);
-    const randomUser = await User.findOne({_id: {$ne: user1Id} }, { _id:1, firstname:1, lastname:1, username:1, photo:1 }).skip(random);
+    const user1 = await User.findOne(
+      { _id: user1Id },
+      { _id: 1, firstname: 1, lastname: 1, username: 1, photo: 1 }
+    );
+    let count = await User.countDocuments({ _id: { $ne: user1Id } });
+    let random = randInt(0, count - 1);
+    const randomUser = await User.findOne(
+      { _id: { $ne: user1Id } },
+      { _id: 1, firstname: 1, lastname: 1, username: 1, photo: 1 }
+    ).skip(random);
     const user2Id = randomUser._id;
-    
+
     //step2: get <NUMBER_OF_PROBLEM> question for both user (both user should never seen all questions before)
-    for (i=0; i<NUMBER_OF_PROBLEM; i++) {
+    for (i = 0; i < NUMBER_OF_PROBLEM; i++) {
       problem = await Problem.findOneAndUpdate(
         {
           subtopicName: subtopicName,
           difficulty: difficulty,
-          users: { $nin: [user1Id,user2Id] },
+          users: { $nin: [user1Id, user2Id] },
         },
-        { $addToSet: { users: [user1Id,user2Id] } },
+        { $addToSet: { users: [user1Id, user2Id] } },
         { projection: { _id: 1 } }
       );
       if (problem == null) {
@@ -46,15 +53,15 @@ exports.randomChallenge = async (req, res) => {
           {
             subtopicName: subtopicName,
             difficulty: difficulty,
-            users: { $nin: [user1Id,user2Id] },
+            users: { $nin: [user1Id, user2Id] },
           },
-          { $addToSet: { users: [user1Id,user2Id] } },
+          { $addToSet: { users: [user1Id, user2Id] } },
           { projection: { _id: 1 } }
         );
       }
       problems.push(problem._id);
     }
-  
+
     //step3: create challenge
     const challenge = new Challenge({
       user1Id: user1Id,
@@ -62,15 +69,18 @@ exports.randomChallenge = async (req, res) => {
       problems: problems,
       subtopicName: subtopicName,
       difficulty: difficulty,
-    })
+    });
 
     //save to database
     await challenge.save();
-    return res.status(200).json({ success: true, data: {challengeId: challenge._id, user1, user2: randomUser}});
+    return res.status(200).json({
+      success: true,
+      data: { challengeId: challenge._id, user1, user2: randomUser },
+    });
   } catch (err) {
     return res.status(400).json({ success: false, error: err });
   }
-}
+};
 
 exports.specificChallenge = async (req, res) => {
   const user1Id = req.body.user1Id;
@@ -78,24 +88,37 @@ exports.specificChallenge = async (req, res) => {
   const subject = req.body.subject;
   const subtopicName = req.body.subtopicName;
   const difficulty = req.body.difficulty;
-  var problem,problems = [];
+  var problem,
+    problems = [];
   try {
     //step1: check if username is exist and not same as user1
-    const user1 = await User.findOne({ _id: user1Id }, { _id:1, firstname:1, lastname:1, username:1, photo:1 });
-    const user2 = await User.findOne({ username: username }, { _id:1, firstname:1, lastname:1, username:1, photo:1 });
-    if (!user2) return res.status(400).json({success: false, error: "Cannot find the user"});
-    if (user2._id == user1Id) return res.status(400).json({success: false, error: "Cannot challenge yourself"});
+    const user1 = await User.findOne(
+      { _id: user1Id },
+      { _id: 1, firstname: 1, lastname: 1, username: 1, photo: 1 }
+    );
+    const user2 = await User.findOne(
+      { username: username },
+      { _id: 1, firstname: 1, lastname: 1, username: 1, photo: 1 }
+    );
+    if (!user2)
+      return res
+        .status(400)
+        .json({ success: false, error: "Cannot find the user" });
+    if (user2._id == user1Id)
+      return res
+        .status(400)
+        .json({ success: false, error: "Cannot challenge yourself" });
     const user2Id = user2._id;
 
     //step2: get <NUMBER_OF_PROBLEM> question for both user (both user should never seen all questions before)
-    for (i=0; i<NUMBER_OF_PROBLEM; i++) {
+    for (i = 0; i < NUMBER_OF_PROBLEM; i++) {
       problem = await Problem.findOneAndUpdate(
         {
           subtopicName: subtopicName,
           difficulty: difficulty,
-          users: { $nin: [user1Id,user2Id] },
+          users: { $nin: [user1Id, user2Id] },
         },
-        { $addToSet: { users: [user1Id,user2Id] } },
+        { $addToSet: { users: [user1Id, user2Id] } },
         { projection: { _id: 1 } }
       );
       if (problem == null) {
@@ -112,9 +135,9 @@ exports.specificChallenge = async (req, res) => {
           {
             subtopicName: subtopicName,
             difficulty: difficulty,
-            users: { $nin: [user1Id,user2Id] },
+            users: { $nin: [user1Id, user2Id] },
           },
-          { $addToSet: { users: [user1Id,user2Id] } },
+          { $addToSet: { users: [user1Id, user2Id] } },
           { projection: { _id: 1 } }
         );
       }
@@ -128,12 +151,24 @@ exports.specificChallenge = async (req, res) => {
       problems: problems,
       subtopicName: subtopicName,
       difficulty: difficulty,
-    })
+    });
 
     // save to database
     await challenge.save();
-    return res.status(200).json({ success: true, data: {challengeId: challenge._id, user1, user2}});
+    return res.status(200).json({
+      success: true,
+      data: { challengeId: challenge._id, user1, user2 },
+    });
   } catch (err) {
     return res.status(400).json({ success: false, error: err });
   }
-}
+};
+
+exports.getProblemByChallengeId = (req, res) => {
+  const challengeId = req.body.challenge_id;
+  Challenge.findById(challengeId)
+    .exec()
+    .then((challenge) => {
+      res.send({ success: true, data: challengeId });
+    });
+};
--- routes/challenge.js
+++ routes/challenge.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-const challengeController = require('../controllers/challenge');
+const challengeController = require("../controllers/challenge");
 
-const { authJwt, adminOnly } = require('../middlewares');
+const { authJwt, adminOnly } = require("../middlewares");
 
 const router = express.Router();
 
-router.post('/random-challenge', [authJwt], challengeController.randomChallenge);
-router.post('/specific-challenge', [authJwt], challengeController.specificChallenge);
+router.get(
+  "/get-problems",
+  [authJwt],
+  challengeController.getProblemByChallengeId
+);
+router.post(
+  "/random-challenge",
+  [authJwt],
+  challengeController.randomChallenge
+);
+router.post(
+  "/specific-challenge",
+  [authJwt],
+  challengeController.specificChallenge
+);
 
 module.exports = router;
