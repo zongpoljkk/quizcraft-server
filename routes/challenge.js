4754c552531c62493ae036c628a66ccc26a854ff
Zongpol <levatinez@gmail.com>
2021-01-01 16:12:28 +0700

    WIP

--- routes/challenge.js
+++ routes/challenge.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-const challengeController = require('../controllers/challenge');
+const challengeController = require("../controllers/challenge");
 
-const { authJwt, adminOnly } = require('../middlewares');
+const { authJwt, adminOnly } = require("../middlewares");
 
 const router = express.Router();
 
-router.post('/random-challenge', [authJwt], challengeController.randomChallenge);
-router.post('/specific-challenge', [authJwt], challengeController.specificChallenge);
+router.post(
+  "/random-challenge",
+  [authJwt],
+  challengeController.randomChallenge
+);
+router.post(
+  "/specific-challenge",
+  [authJwt],
+  challengeController.specificChallenge
+);
+router.delete(
+  "/delete-challenge",
+  [authJwt],
+  challengeController.deleteChallenge
+);
 
 module.exports = router;
