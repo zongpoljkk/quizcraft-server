7317fc07ddd38771d7f293814de36a6975dd6d4a
mingokl02135 <mingbudsakorn@gmail.com>
2021-01-04 17:31:08 +0700

    init

--- controllers/mathProblem/meaningOfExponents.js
+++ controllers/mathProblem/meaningOfExponents.js
@@ -1,14 +1,29 @@
 const Problem = require("../../models/Problem");
 const Answer = require("../../models/Answer");
 const Hint = require("../../models/Hint");
+const math = require("mathjs");
 const EASY = "EASY";
 const MEDIUM = "MEDIUM";
 const HARD = "HARD";
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+const randInt = (start, end, haveNegative) => {
+  if (haveNegative) {
+    return (
+      (Math.floor(Math.random() * (end - start + 1)) + start) *
+      (-1) ** Math.floor(Math.random() * 2)
+    );
+  } else {
+    return Math.floor(Math.random() * (end - start + 1)) + start;
+  }
+};
+
 const generateMeaningOfExponents = async (subtopicName, difficulty) => {
+  var problemTitle,problemBody,answerBody,hintBody,solution;
+  var expo, num;
   switch (difficulty) {
     case EASY:
+
       return "Not Implement";
     case MEDIUM:
       return "Not Implement";
