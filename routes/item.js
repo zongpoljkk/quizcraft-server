f38165a9eea93694f95b6e287de9ac79010000af
mingokl02135 <mingbudsakorn@gmail.com>
2021-01-19 19:10:10 +0700

    del skip challenge

--- routes/item.js
+++ routes/item.js
@@ -8,7 +8,6 @@ const router = express.Router();
 
 router.get("/", [authJwt], itemController.getAllItems);
 router.post("/add-item", [authJwt, adminOnly], itemController.addItem);
-router.post("/use-skip-item-for-challenge", [authJwt], itemController.useSkipItemForChallenge);
 router.post("/use-skip-item-for-quiz", [authJwt], itemController.useSkipItemForQuiz);
 router.post("/use-refresh-item", [authJwt], itemController.useRefreshItem);
 
