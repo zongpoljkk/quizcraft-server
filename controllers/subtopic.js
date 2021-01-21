4c5a04e095ebaae615d136a236dd15664d73abd4
Zongpol <levatinez@gmail.com>
2021-01-21 13:50:03 +0700

    WIP

--- controllers/subtopic.js
+++ controllers/subtopic.js
@@ -5,7 +5,7 @@ exports.addFile = (req, res) => {
   const subtopicName = req.body.subtopicName;
   const subjectImage = req.files.subjectImage;
   const topicImage = req.files.topicImage;
-  Subtopic.findOne({subtopicName: subtopicName})
+  Subtopic.findOne({ subtopicName: subtopicName })
     .select("_id photo")
     .exec((err, subtopic) => {
       if (err) {
@@ -21,72 +21,27 @@ exports.addFile = (req, res) => {
       subtopic.subjectImg = subjectImage;
       subtopic.topicImg = topicImage;
       subtopic.save();
-      res.status(200).send({ success: true, data: "Upload subtopic media succeeded" });
+      res
+        .status(200)
+        .send({ success: true, data: "Upload subtopic media succeeded" });
     });
 };
 
 exports.getAllSubtopics = async (req, res) => {
-  // try {
-  //   await Achievement.aggregate(
-  //     [
-  //       {
-  //         $lookup: {
-  //           from: "media.chunks",
-  //           localField: "image.id",
-  //           foreignField: "files_id",
-  //           as: "image_info",
-  //         },
-  //       },
-  //       { $unwind: "$image_info" },
-  //       {
-  //         $lookup: {
-  //           from: "media.chunks",
-  //           localField: "lottie.id",
-  //           foreignField: "files_id",
-  //           as: "lottie_info",
-  //         },
-  //       },
-  //       { $unwind: "$lottie_info" },
-  //       {
-  //         $project: {
-  //           _id: 1,
-  //           name: 1,
-  //           description: 1,
-  //           "image_info.data": 1,
-  //           "lottie_info.data": 1,
-  //         },
-  //       },
-  //     ],
-  //     (err, achievements) => {
-  //       if (err) {
-  //         return res.status(500).json({ success: false, error: err });
-  //       }
-  //       if (!achievements.length) {
-  //         return res
-  //           .status(400)
-  //           .json({ success: false, data: "no achievements" });
-  //       }
-  //       return res.status(200).json({ success: true, data: achievements });
-  //     }
-  //   );
-  // } catch (err) {
-  //   console.log(err);
-  // }
-  await Subtopic.find()
-    .exec((err, subtopics) => {
-      if (err) {
-        return res.status(500).json({ success: false, error: err });
-      }
-      if (!subtopics.length) {
-        return res.status(400).json({ success: false, data: "no data" });
-      }
-      return res.status(200).json({ success: true, data: subtopics });
-    })
+  await Subtopic.find().exec((err, subtopics) => {
+    if (err) {
+      return res.status(500).json({ success: false, error: err });
+    }
+    if (!subtopics.length) {
+      return res.status(400).json({ success: false, data: "no data" });
+    }
+    return res.status(200).json({ success: true, data: subtopics });
+  });
 };
 
 exports.getAllSubjects = async (req, res) => {
   // TODO: Return subjects image
-   // try {
+  // try {
   //   await Achievement.aggregate(
   //     [
   //       {
@@ -151,64 +106,130 @@ exports.getAllSubjects = async (req, res) => {
 
 exports.getTopicBySubjectName = async (req, res) => {
   const subject = req.query.subject;
-  await Subtopic.aggregate(
-    [{
-      $match: {
-        subject: subject,
-      },
-    },{
-      $group: { 
-        _id: "$topic", 
-        topicImg: { $first: "$topicImg" } 
-      } 
-    }],
-    (err, topic) => {
-      if (err) {
-        return res.status(500).json({ success: false, error: err });
+  try {
+    await Subtopic.aggregate(
+      [
+        {
+          $match: {
+            subject: subject,
+          },
+        },
+        {
+          $group: {
+            _id: "$topic",
+            topicImg: { $first: "$topicImg" },
+          },
+        },
+        // {
+        //   $lookup: {
+        //     from: "media.chunks",
+        //     localField: "subjectImg.id",
+        //     foreignField: "files_id",
+        //     as: "subject_image_info",
+        //   },
+        // },
+        // { $unwind: "$subject_image_info" },
+        // {
+        //   $lookup: {
+        //     from: "media.chunks",
+        //     localField: "topicImg.id",
+        //     foreignField: "files_id",
+        //     as: "topic_image_info",
+        //   },
+        // },
+        // { $unwind: "$lottie_info" },
+        // {
+        //   $project: {
+        //     _id: 1,
+        //     subtopicName: 1,
+        //     subject: 1,
+        //     topic: 1,
+        //     availableDifficulty: 1,
+        //     "subject_image_info.data": 1,
+        //     "topic_image_info.data": 1,
+        //   },
+        // },
+      ],
+      (err, subtopics) => {
+        console.log(subtopics);
+        if (err) {
+          return res.status(500).json({ success: false, error: err });
+        }
+        if (!subtopics.length) {
+          return res.status(400).json({ success: false, data: "no subtopics" });
+        }
+        return res.status(200).json({ success: true, data: subtopics });
       }
-      if (!topic.length) {
-        return res.status(400).json({ success: false, data: "no topics" });
-      }
-      return res.status(200).json({ success: true, data: topic });
-    })
+    );
+  } catch (err) {
+    console.log(err);
+  }
+  console.log("FINALLY");
+  // await Subtopic.aggregate(
+  // [{
+  //   $match: {
+  //     subject: subject,
+  //   },
+  // },{
+  //   $group: {
+  //     _id: "$topic",
+  //     topicImg: { $first: "$topicImg" }
+  //   }
+  // }],
+  //   (err, topic) => {
+  //     if (err) {
+  //       return res.status(500).json({ success: false, error: err });
+  //     }
+  //     if (!topic.length) {
+  //       return res.status(400).json({ success: false, data: "no topics" });
+  //     }
+  //     return res.status(200).json({ success: true, data: topic });
+  //   })
 };
 
 exports.getSubtopicByTopicName = async (req, res) => {
   var availableDifficulty;
   var subtopic;
   try {
-    const subtopics = await Subtopic.find({ topic: req.query.topic }, { subtopicName: 1, availableDifficulty:1 });
+    const subtopics = await Subtopic.find(
+      { topic: req.query.topic },
+      { subtopicName: 1, availableDifficulty: 1 }
+    );
     for (i in subtopics) {
       subtopic = subtopics[i];
       availableDifficulty = [
         {
           isAvailable: false,
-          difficulty: DIFFICULTY.EASY
+          difficulty: DIFFICULTY.EASY,
         },
         {
           isAvailable: false,
-          difficulty: DIFFICULTY.MEDIUM
+          difficulty: DIFFICULTY.MEDIUM,
         },
         {
           isAvailable: false,
-          difficulty: DIFFICULTY.HARD
-        }
-      ]
+          difficulty: DIFFICULTY.HARD,
+        },
+      ];
       for (j in subtopic.availableDifficulty) {
         if (subtopic.availableDifficulty[j].difficulty == DIFFICULTY.EASY) {
-          availableDifficulty[0].isAvailable = subtopic.availableDifficulty[j].isAvailable;
-        } 
-        else if (subtopic.availableDifficulty[j].difficulty == DIFFICULTY.MEDIUM) {
-          availableDifficulty[1].isAvailable = subtopic.availableDifficulty[j].isAvailable;
-        } 
-        else if (subtopic.availableDifficulty[j].difficulty == DIFFICULTY.HARD) {
-          availableDifficulty[2].isAvailable = subtopic.availableDifficulty[j].isAvailable;
+          availableDifficulty[0].isAvailable =
+            subtopic.availableDifficulty[j].isAvailable;
+        } else if (
+          subtopic.availableDifficulty[j].difficulty == DIFFICULTY.MEDIUM
+        ) {
+          availableDifficulty[1].isAvailable =
+            subtopic.availableDifficulty[j].isAvailable;
+        } else if (
+          subtopic.availableDifficulty[j].difficulty == DIFFICULTY.HARD
+        ) {
+          availableDifficulty[2].isAvailable =
+            subtopic.availableDifficulty[j].isAvailable;
         }
       }
       subtopics[i].availableDifficulty = availableDifficulty;
     }
     return res.status(200).json({ success: true, data: subtopics });
-
   } catch (err) {
     return res.status(500).json({ success: false, error: err.toString() });
   }
@@ -231,8 +252,12 @@ exports.updateAvailableDifficulty = async (req, res) => {
   await Subtopic.findOne({
     subtopicName: subtopicName,
   }).exec((err, subtopic) => {
-    if (err) return res.status(500).json({ success: false, error: err.toString() });
-    else if (!subtopic) return res.status(400).json({ success: false, error: "Subtopic not exist" });
+    if (err)
+      return res.status(500).json({ success: false, error: err.toString() });
+    else if (!subtopic)
+      return res
+        .status(400)
+        .json({ success: false, error: "Subtopic not exist" });
     let have = false;
     for (i in subtopic.availableDifficulty) {
       if (subtopic.availableDifficulty[i].difficulty == difficulty) {
@@ -245,40 +270,55 @@ exports.updateAvailableDifficulty = async (req, res) => {
       subtopic.availableDifficulty.push({
         difficulty: difficulty,
         isAvailable: isAvailable,
-      })
+      });
     }
     subtopic.save((err, newSubtopic) => {
-      if (err) return res.status(500).json({ success: false, error: err.toString() });
-      else if (!newSubtopic) return res.status(400).json({ success: false, error: "Cannot update subtopic available difficulty" });
+      if (err)
+        return res.status(500).json({ success: false, error: err.toString() });
+      else if (!newSubtopic)
+        return res
+          .status(400)
+          .json({
+            success: false,
+            error: "Cannot update subtopic available difficulty",
+          });
       return res.status(200).json({ success: true, newSubtopic });
-    })
-  })
-}
+    });
+  });
+};
 
-exports.getAvailableDifficultyBySubtopicName = async (req,res) => {
+exports.getAvailableDifficultyBySubtopicName = async (req, res) => {
   const subtopicName = req.query.subtopicName;
   const availableDifficulty = {
-    EASY: false, 
-    MEDIUM: false, 
-    HARD: false
-  }
+    EASY: false,
+    MEDIUM: false,
+    HARD: false,
+  };
   try {
-    const subtopic = await Subtopic.findOne({ subtopicName: subtopicName }, {availableDifficulty: 1, _id: 0});
-    if (!subtopic) return res.status(400).json({ success: false, error: "Cannot find this subtopic" });
+    const subtopic = await Subtopic.findOne(
+      { subtopicName: subtopicName },
+      { availableDifficulty: 1, _id: 0 }
+    );
+    if (!subtopic)
+      return res
+        .status(400)
+        .json({ success: false, error: "Cannot find this subtopic" });
     for (i in subtopic.availableDifficulty) {
       if (subtopic.availableDifficulty[i].difficulty == DIFFICULTY.EASY) {
         availableDifficulty.EASY = subtopic.availableDifficulty[i].isAvailable;
-      } 
-      else if (subtopic.availableDifficulty[i].difficulty == DIFFICULTY.MEDIUM) {
-        availableDifficulty.MEDIUM = subtopic.availableDifficulty[i].isAvailable;
-      } 
-      else if (subtopic.availableDifficulty[i].difficulty == DIFFICULTY.HARD) {
+      } else if (
+        subtopic.availableDifficulty[i].difficulty == DIFFICULTY.MEDIUM
+      ) {
+        availableDifficulty.MEDIUM =
+          subtopic.availableDifficulty[i].isAvailable;
+      } else if (
+        subtopic.availableDifficulty[i].difficulty == DIFFICULTY.HARD
+      ) {
         availableDifficulty.HARD = subtopic.availableDifficulty[i].isAvailable;
       }
     }
-    return res.status(200).json({ success: true, data: availableDifficulty});
+    return res.status(200).json({ success: true, data: availableDifficulty });
   } catch (err) {
     return res.status(500).json({ success: false, error: err.toString() });
   }
-}
-
+};
