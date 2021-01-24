b0f4b7af07653e3ac4c434819a75e1cfc3940f86
mingokl02135 <mingbudsakorn@gmail.com>
2021-01-25 00:07:02 +0700

    del old stn and del non-use const

--- controllers/mathProblem/const.js
+++ controllers/mathProblem/const.js
@@ -1,17 +1,3 @@
-exports.EASY = "EASY";
-
-exports.MEDIUM = "MEDIUM";
-
-exports.HARD = "HARD";
-
-exports.SELECT_ONE = "SELECT_ONE";
-
-exports.RADIO_CHOICE = "RADIO_CHOICE";
-
-exports.MATH_INPUT = "MATH_INPUT";
-
-exports.alphabet = "abcdefghijklmnopqrstuvwxyz";
-
 exports.SUPTOPIC = {
   MEANING_OF_EXPONENTS: "ความหมายของเลขยกกำลัง",
   OPERATION_OF_EXPONENTS: "การดำเนินการของเลขยกกำลัง",
