0ae4c1f9581d4b22202586dc8d58be8fb98d1813
mingokl02135 <mingbudsakorn@gmail.com>
2021-01-26 00:14:35 +0700

    edit get hint for new schema and save history of use hint item

--- controllers/hint.js
+++ controllers/hint.js
@@ -1,16 +1,53 @@
-const Hint = require('../models/Hint');
+const Problem = require("../models/Problem");
+const User = require("../models/User");
+const { ITEM_NAME } = require("../utils/const");
 
-exports.getHintByProblemId = (req, res, next) => {
-  Hint.findOne({ problemId: req.query.problemId }, (err, hint) => {
-    if(err) {
-      return res.status(500).json({ success: false, error: err })
+exports.getHintByProblemId = async (req, res) => {
+  const userId = req.userId;
+  const problemId = req.query.problemId;
+  var user = await User.findOne(
+    { _id: userId }, 
+    { items:1, usedItems:1}
+  );
+
+  var problem = await Problem.findById(problemId);
+  if (!problem) {
+    return res.status(400).json({ success: false, error: "problem not found" });
+  }
+  let foundUsedItem = false;
+  for (i in problem.usedItems) {
+    if (problem.usedItems[i].itemName = ITEM_NAME.HINT) {
+      problem.usedItems[i].amount++;
+      foundUsedItem = true;
+      break;
     }
-    else if (!hint) {
-      return res
-        .status(400)
-        .json({ success: false, error: `Hint not found` })
+  }
+  if (!foundUsedItem) {
+    problem.usedItems.push({
+      itemName: ITEM_NAME.HINT,
+      amount: 1
+    })
+  }
+
+  // * Save used item * //
+  foundUsedItem = false;
+  for (i in user.usedItems) {
+    if (user.usedItems[i].itemName = ITEM_NAME.HINT) {
+      user.usedItems[i].problems.push(problemId);
+      user.usedItems[i].amount++;
+      foundUsedItem = true;
+      break;
     }
-    else return res.status(200).json({ success: true, data: hint })
-    next();
-  });
+  }
+  if (!foundUsedItem) {
+    user.usedItems.push({
+      itemName: ITEM_NAME.HINT,
+      amount: 1,
+      problems: problemId
+    })
+  }
+  //save to database
+  await user.save();
+  await problem.save();
+  return res.status(200).json({ success: true, data: { problemId: problemId, body: problem.hintBody }});
 }
--- controllers/item.js
+++ controllers/item.js
@@ -178,11 +178,11 @@ exports.useRefreshItem = async (req,res) => {
     
     var problem = await Problem.findOne({ _id: problemId });
     if (!problem) {
-      return res.status(400).json({ success: false, error: "problem not founded" });
+      return res.status(400).json({ success: false, error: "problem not found" });
     }
     let foundUsedItem = false;
     for (i in problem.usedItems) {
-      if (problem.usedItems[i].itemName = ITEM_NAME.SKIP) {
+      if (problem.usedItems[i].itemName = ITEM_NAME.REFRESH) {
         problem.usedItems[i].amount++;
         foundUsedItem = true;
         break;
@@ -190,7 +190,7 @@ exports.useRefreshItem = async (req,res) => {
     }
     if (!foundUsedItem) {
       problem.usedItems.push({
-        itemName: ITEM_NAME.SKIP,
+        itemName: ITEM_NAME.REFRESH,
         amount: 1
       })
     }
