6ec61f4a08b41fe7014bd211e206fb50d81ab002
mingokl02135 <mingbudsakorn@gmail.com>
2021-01-25 23:46:34 +0700

    del get all hint

--- routes/hint.js
+++ routes/hint.js
@@ -6,7 +6,6 @@ const { authJwt, adminOnly } = require('../middlewares');
 
 const router = express.Router();
 
-router.get('/', [authJwt], hintController.getAllHints)
 router.get('/get-hint/', [authJwt], hintController.getHintByProblemId)
 
 module.exports = router;
