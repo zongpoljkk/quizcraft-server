7faeff18097856ca6c7669e745ccc9d7496a0729
mingokl02135 <mingbudsakorn@gmail.com>
2021-01-26 18:08:55 +0700

    sse and nextProblem endpoint

--- routes/group.js
+++ routes/group.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const { re } = require("mathjs");
 
 const groupController = require("../controllers/group");
 
-const { authJwt, adminOnly } = require("../middlewares");
+const { 
+  authJwt, 
+  adminOnly, 
+  groupEventsHandler,
+  subscribers,
+  closeConnection 
+} = require("../middlewares");
 
 const router = express.Router();
 
@@ -14,5 +21,10 @@ router.post("/gen-problems-when-group-start", [authJwt], groupController.genProb
 router.get("/group-scoreboard/", [authJwt], groupController.getGroupScoreboard);
 router.put("/join-group", [authJwt], groupController.joinGroup);
 router.get("/get-group-game", [authJwt], groupController.getGroupGame);
+router.post("/next-problem", [authJwt], groupController.nextProblem);
+router.get("/event/", [authJwt], groupEventsHandler);
+router.delete("/close/", [authJwt], closeConnection);
+//for test
+router.get("/status", (req, res) => res.json({ clients: subscribers.length }));
 
 module.exports = router;
