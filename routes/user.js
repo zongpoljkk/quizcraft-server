e123562bdb5b71f357e824112266e3fccaaee9e0
nnnnnnnutt <pimkunut.tee@gmail.com>
2021-02-08 18:36:26 +0700

    forget to add auth

--- routes/user.js
+++ routes/user.js
@@ -34,7 +34,7 @@ const storage = new GridFsStorage({
 const upload = multer({ storage });
 
 router.get("/", [authJwt], userController.getAllUsers);
-router.get("/get-user/", userController.getProfileByUID);
+router.get("/get-user/", [authJwt], userController.getProfileByUID);
 router.get("/get-amount-of-items", [authJwt], userController.getAmountOfItems);
 router.put("/edit-username", [authJwt], userController.editUsername);
 router.post("/add-user", [authJwt, adminOnly], userController.addUser);
