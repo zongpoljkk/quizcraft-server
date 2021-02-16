578b78ede5449dc46cd0978cee74d600805c1827
Zongpol <levatinez@gmail.com>
2021-02-17 00:14:54 +0700

    fix: check if user has photo before deleting

--- controllers/user.js
+++ controllers/user.js
@@ -371,8 +371,10 @@ exports.changeProfilePicture = (req, res, next) => {
         });
       }
 
-      deleteProfilePicture('uploads.files', { _id: ObjectId(user.photo.id)});
-      deleteProfilePicture('uploads.chunks', { files_id: ObjectId(user.photo.id)});
+      if (user.photo) {
+        deleteProfilePicture('uploads.files', { _id: ObjectId(user.photo.id)});
+        deleteProfilePicture('uploads.chunks', { files_id: ObjectId(user.photo.id)});
+      }
 
       user.photo = req.file;
       user.save();
